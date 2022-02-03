28841a0971cc89e786edee80d029e4be11e815e1
Jinbo <jinboom@foxmail.com>
2022-02-03 14:52:41 +0800

    [feat][factory] add factory method design pattern

---
+++ factory/factoryMethod/NYPizzaStore.hpp
@@ -0,0 +1,27 @@
+#ifndef NYPIZZASTORE_HPP
+#define NYPIZZASTORE_HPP
+
+#include "PizzaStore.hpp"
+#include "NYStyleCheesePizza.hpp"
+#include "NYStylePepperoniPizza.hpp"
+
+class NYPizzaStore : public PizzaStore
+{
+public:
+    std::unique_ptr<Pizza> createPizza(std::string type) override
+    {
+        std::unique_ptr<Pizza> pizza = nullptr;
+
+        if (type == "cheese")
+        {
+            pizza = std::make_unique<NYStyleCheesePizza>();
+        }
+        else if (type == "pepperoni")
+        {
+            pizza = std::make_unique<NYStylePepperoniPizza>();
+        }
+        return pizza;
+    }
+};
+
+#endif
